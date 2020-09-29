b972b88ab750bf77533ab86849b93ee10341f452
Jungwoo An <o1o9814@naver.com>
2020-09-30 00:32:29 +0900

    feat: Add shared enum
    NodeTypes, TagTypes

---
+++ packages/shared/src/enum.ts
@@ -0,0 +1,25 @@
+export const enum NodeTypes {
+  TEXT = 'TEXT',
+  ELEMENT = 'ELEMENT',
+  COMMENT = 'COMMENT',
+}
+
+export const enum TagTypes {
+  // Blocks
+  ACTIONS = 'actions',
+  CONTEXT = 'context',
+  DIVIDER = 'divider',
+  HEADER = 'header',
+  IMAGE = 'image',
+  SECTION = 'section',
+  SECTION_ACCESSORY = 'section-accessory',
+  SECTION_FIELDS = 'section-fields',
+
+  // Block Elements
+  BUTTON = 'button',
+  DATE_PICKER = 'date-picker',
+  SELECT_MENU = 'select-menu',
+
+  // Composition Objects
+  TEXT = 'text',
+}
