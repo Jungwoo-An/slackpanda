a0a37cef0c2c24ef6f042befc910d19da4152e30
Jungwoo An <o1o9814@naver.com>
2020-09-30 00:50:50 +0900

    feat: Add nextSibling function of renderer

---
+++ packages/vue/src/renderer/next-sibling.ts
@@ -0,0 +1,12 @@
+import { Node } from '@spd/shared';
+
+function nextSibling(node: Node): Node | null {
+  const { parentNode } = node;
+  if (!parentNode) {
+    return null;
+  }
+
+  return parentNode.children[parentNode.children.indexOf(node) + 1] || null;
+}
+
+export default nextSibling;
