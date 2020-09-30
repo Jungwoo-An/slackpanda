c032bf34af8c63e8f5f07739589ae2303faad337
Jungwoo An <o1o9814@naver.com>
2020-09-30 13:54:27 +0900

    refactor: Update some schemas
    Change snake_case to camelCase for flexible code sharing between packages

--- packages/schema/src/button.ts
+++ packages/schema/src/button.ts
@@ -1,7 +1,7 @@
 import Joi from 'joi';
 
 export default Joi.object({
-  action_id: Joi.string().required(),
+  actionId: Joi.string().required(),
   value: Joi.any().required(),
   url: Joi.string(),
   style: Joi.string(),
--- packages/schema/src/date-picker.ts
+++ packages/schema/src/date-picker.ts
@@ -1,7 +1,7 @@
 import Joi from 'joi';
 
 export default Joi.object({
-  action_id: Joi.string().required(),
+  actionId: Joi.string().required(),
   placeholder: Joi.string(),
   initialDate: Joi.string(),
 });
