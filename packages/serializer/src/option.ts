967c3c2f1c7bf058a0f3843589ba27b75052b126
Jungwoo An <o1o9814@naver.com>
2020-09-30 16:42:27 +0900

    feat: Add option serializer

---
+++ packages/serializer/src/option.ts
@@ -0,0 +1,26 @@
+import { IElement, NodeTypes } from '@spd/shared';
+
+import serialize from '.';
+
+function option(node: IElement) {
+  const text = node.children.find((child) => child.type === NodeTypes.TEXT);
+  if (!text) {
+    return null;
+  }
+
+  return {
+    text: {
+      type: 'plain_text',
+      text: serialize(text),
+    },
+    value: node.props.value,
+    ...(node.props.description && {
+      description: node.props.description,
+    }),
+    ...(node.props.url && {
+      url: node.props.url,
+    }),
+  };
+}
+
+export default option;
