19e873662cca332cc6c08cd27057099924338674
Jungwoo An <o1o9814@naver.com>
2020-09-30 13:48:41 +0900

    refactor: Update select-menu schema
    Delete unused key

--- packages/schema/src/select-menu.ts
+++ packages/schema/src/select-menu.ts
@@ -4,5 +4,4 @@ export default Joi.object({
   action_id: Joi.string().required(),
   placeholder: Joi.string().required(),
   value: Joi.any().required(),
-  multiple: Joi.boolean().required(),
 });
