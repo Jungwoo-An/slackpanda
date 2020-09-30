8b2ab96b23a87676753453337ad732c9d7386ee4
Jungwoo An <o1o9814@naver.com>
2020-09-30 13:16:56 +0900

    refactor: Update schema package index
    Change based on TagTypes

--- packages/schema/src/index.ts
+++ packages/schema/src/index.ts
@@ -1,11 +1,32 @@
-export { default as actions } from './actions';
-export { default as button } from './button';
-export { default as context } from './context';
-export { default as datePicker } from './date-picker';
-export { default as divider } from './divider';
-export { default as header } from './header';
-export { default as image } from './image';
-export { default as sectionAccessory } from './section-accessory';
-export { default as sectionFields } from './section-fields';
-export { default as section } from './section';
-export { default as selectMenu } from './select-menu';
+import { TagTypes } from '@spd/shared';
+import Joi, { date } from 'joi';
+
+import actions from './actions';
+import button from './button';
+import context from './context';
+import datePicker from './date-picker';
+import divider from './divider';
+import header from './header';
+import image from './image';
+import sectionAccessory from './section-accessory';
+import sectionFields from './section-fields';
+import section from './section';
+import selectMenu from './select-menu';
+import text from './text';
+
+const schema: Record<TagTypes, Joi.ObjectSchema> = {
+  [TagTypes.ACTIONS]: actions,
+  [TagTypes.BUTTON]: button,
+  [TagTypes.CONTEXT]: context,
+  [TagTypes.DATE_PICKER]: datePicker,
+  [TagTypes.DIVIDER]: divider,
+  [TagTypes.HEADER]: header,
+  [TagTypes.IMAGE]: image,
+  [TagTypes.SECTION_ACCESSORY]: sectionAccessory,
+  [TagTypes.SECTION_FIELDS]: sectionFields,
+  [TagTypes.SECTION]: section,
+  [TagTypes.SELECT_MENU]: selectMenu,
+  [TagTypes.TEXT]: text,
+};
+
+export default schema;
