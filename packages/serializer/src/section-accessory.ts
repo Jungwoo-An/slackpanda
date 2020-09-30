c2753bc2916a984f86ce95d050c1a7f8aeaab074
Jungwoo An <o1o9814@naver.com>
2020-09-30 16:42:35 +0900

    feat: Add section-accessory serializer

---
+++ packages/serializer/src/section-accessory.ts
@@ -0,0 +1,9 @@
+import { IElement } from '@spd/shared';
+
+import serialize from '.';
+
+function sectionAccessory(node: IElement) {
+  return serialize(node.children[0]);
+}
+
+export default sectionAccessory;
