6b223e8e8563c2a72637df9febab4eaea44c6b14
Jungwoo An <o1o9814@naver.com>
2020-12-03 00:56:11 +0900

    feat: Add interactive message handler
    @slack/interactive-messages 를 한 번 더 감싸서 BaseClient 를 상속하는 클래스에서 인터랙션을 핸들링할 수 있는 구조로 만듦

--- packages/core/src/client/base.ts
+++ packages/core/src/client/base.ts
@@ -1,13 +1,58 @@
 import { EventEmitter } from 'events';
 
 import { WebClient } from '@slack/web-api';
+import { createMessageAdapter } from '@slack/interactive-messages';
+import { FixMe } from '@spd/shared';
 
-export class BaseClient extends EventEmitter {
+// TODO :: Change class name to SlackClient
+export abstract class BaseClient extends EventEmitter {
   protected _instance: WebClient;
 
-  constructor({ apiToken }: { apiToken: string }) {
+  protected _signingSecret?: string;
+
+  protected _adapter?: ReturnType<typeof createMessageAdapter>;
+
+  constructor({
+    apiToken,
+    signingSecret,
+  }: {
+    apiToken: string;
+    signingSecret?: string;
+  }) {
     super();
 
     this._instance = new WebClient(apiToken);
+
+    if (signingSecret) {
+      this._adapter = this.initAdapter(signingSecret);
+    }
+  }
+
+  protected abstract handleAction(payload: FixMe): void | Promise<void>;
+
+  private initAdapter(signingSecret: string) {
+    const adapter = createMessageAdapter(signingSecret);
+
+    adapter.action(/.*/, this.handleAction);
+
+    return adapter;
+  }
+
+  public start(port: number) {
+    if (!this._adapter) {
+      // TODO :: Custom error class
+      throw new Error('Slack message adapter is not initialize');
+    }
+
+    return this._adapter.start(port);
+  }
+
+  public getRequestHandler() {
+    if (!this._adapter) {
+      // TODO :: Custom Error Class
+      throw new Error('Slack message adapter is not initialize');
+    }
+
+    return this._adapter;
   }
 }
