79f23302bcf3399bd959669fd69a83cd95e1c6bd
Jungwoo An <o1o9814@naver.com>
2020-12-07 23:53:25 +0900

    feat: Add PbModal component

---
+++ packages/vue/src/components/PbModal.vue
@@ -0,0 +1,34 @@
+<template>
+  <modal :title="title" :submit="submit" :close="close" @action="handleAction">
+    <slot />
+  </modal>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'PbModal',
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    submit: {
+      type: String,
+    },
+    close: {
+      type: String,
+    },
+  },
+  setup(_, { emit }) {
+    function handleAction() {
+      emit('submit');
+    }
+
+    return {
+      handleAction,
+    };
+  },
+});
+</script>
