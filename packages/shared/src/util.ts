e34fa30c821f9782a46885719c07f132b48cdd30
Jungwoo An <o1o9814@naver.com>
2020-12-14 23:13:06 +0900

    feat: Add omitModal, findModal
    트리에서 모달을 찾거나, 누락시키기 위한 헬퍼 함수

--- packages/shared/src/util.ts
+++ packages/shared/src/util.ts
@@ -1,4 +1,5 @@
-import { Node } from './interface';
+import { NodeTypes, TagTypes } from './enum';
+import { IElement, Node } from './interface';
 
 export function generateNonce() {
   return (Date.now() + Math.floor(Math.random() * 1000)).toString(16);
@@ -10,4 +11,16 @@ export function findRoot(node: Node): Node {
   }
 
   return node;
+}
+
+export function omitModal(nodes: Node[]) {
+  return nodes.filter(
+    (node) => node.type !== NodeTypes.ELEMENT || node.tag !== TagTypes.MODAL
+  );
+}
+
+export function findModal(nodes: Node[]): IElement | undefined {
+  return nodes.find(
+    (node) => node.type === NodeTypes.ELEMENT && node.tag === TagTypes.MODAL
+  ) as IElement;
 }
