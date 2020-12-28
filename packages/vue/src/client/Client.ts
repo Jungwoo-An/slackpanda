3f6b2b946a2e03c5269a07bb0c571c1e800bf65d
Jungwoo An <o1o9814@naver.com>
2020-12-29 01:45:26 +0900

    refactor: Change vue client class for adapter interface
    어댑터를 사용하는 구조로 변경함 (슬랙뿐만 아니라 타 플랫폼까지 확장할 수 있는 구조로 바꿈)

--- packages/vue/src/client/Client.ts
+++ packages/vue/src/client/Client.ts
@@ -1,99 +1,39 @@
+import { EventEmitter } from 'events';
+
 import { Component } from 'vue';
 import {
   NodeTypes,
   FixMe,
   TagTypes,
   IElement,
-  ActionType,
-  omitModal,
-  findModal,
+  IClientAdapter,
 } from '@spd/shared';
-import { BaseClient, UpdateObserver } from '@spd/core';
-import { serialize } from '@spd/serializer';
+import { UpdateObserver } from '@spd/core';
 
 import { createApp } from '../renderer';
-import { ACTION_HANDLER_STORAGE } from '../storage';
 
-export class Client extends BaseClient {
-  private _observer: UpdateObserver;
+export class Client extends EventEmitter {
+  private _adapter: IClientAdapter;
 
-  private _threads: Map<
-    FixMe,
-    {
-      channel: string;
-      ts: string;
-    }
-  > = new Map();
+  private _updateObserver: UpdateObserver;
 
   constructor({
-    apiToken,
-    observer,
-    signingSecret,
+    adapter,
+    updateObserver,
   }: {
-    apiToken: string;
-    observer: UpdateObserver;
-    signingSecret?: string;
+    adapter: IClientAdapter;
+    updateObserver: UpdateObserver;
   }) {
-    super({
-      apiToken,
-      signingSecret,
-    });
+    super();
 
-    this._observer = observer;
-    this._observer.subscribe(this.handleUpdate);
+    this._adapter = adapter;
+
+    this._updateObserver = updateObserver;
+    this._updateObserver.subscribe(this.handleUpdate);
   }
 
-  protected handleSubmission(payload: FixMe) {
-    const actionId = payload.view.callback_id;
-    const handler = ACTION_HANDLER_STORAGE.get(actionId);
-    return handler?.(payload);
-  }
-
-  protected handleViewClosed(payload: FixMe) {
-    const actionId = payload.view.callback_id;
-    const handler = ACTION_HANDLER_STORAGE.get(actionId);
-    handler?.(payload);
-  }
-
-  protected handleAction(payload: FixMe) {
-    const [action] = (payload?.actions ?? []) as ActionType[];
-    if (!action) {
-      return;
-    }
-
-    const handler = ACTION_HANDLER_STORAGE.get(action.action_id);
-    handler?.(payload, action);
-  }
-
-  private handleUpdate = (app: FixMe) => {
-    const thread = this._threads.get(app);
-    if (!thread) {
-      return;
-    }
-
-    const modal = findModal(app.children);
-
-    const blocks = serialize({
-      ...app,
-      children: omitModal(app.children),
-    });
-
-    this._instance.chat.update({
-      channel: thread.channel,
-      ts: thread.ts,
-      text: '',
-      blocks,
-    });
-
-    if (modal && modal.props.open) {
-      this._instance.views.open({
-        trigger_id: modal.props.triggerId,
-        view: {
-          notify_on_close: true,
-          ...serialize(modal),
-        },
-      });
-    }
+  private handleUpdate = (app: IElement) => {
+    return this._adapter.updateMessage(app);
   };
 
   private render(component: Component) {
@@ -112,37 +52,35 @@ export class Client extends BaseClient {
     return root;
   }
 
+  public addEventListener(event: string, handler: (...args: any[]) => any) {
+    this._adapter.addEventListener(event, handler);
+  }
+
+  public removeEventListener(event: string, handler: (...args: any[]) => any) {
+    this._adapter.removeEventListener(event, handler);
+  }
+
   public async sendMessage(
     textOrComponent: string | Component,
     channel: string
   ) {
     if (typeof textOrComponent === 'string') {
-      this._instance.chat.postMessage({
+      return this._adapter.sendMessage({
         text: textOrComponent,
-        channel,
+        channelId: channel,
       });
-      return undefined;
     }
 
     const app = this.render(textOrComponent);
 
-    const blocks = serialize({
-      ...app,
-      children: omitModal(app.children),
-    });
-
     try {
-      const response = (await this._instance.chat.postMessage({
+      const response = await this._adapter.sendMessage({
         text: '',
-        blocks,
-        channel,
-      })) as FixMe;
+        node: app,
+        channelId: channel,
+      });
 
       (app as FixMe).initialized = true;
-      this._threads.set(app, {
-        channel: response.channel,
-        ts: response.ts,
-      });
 
       return response;
     } catch (e) {
